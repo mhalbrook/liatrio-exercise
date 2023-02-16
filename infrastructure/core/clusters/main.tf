3a6b14c2c8198fe770d0a852f72736429967f61a
mhalbrook <mitch.halbrook@protonmail.com>
2023-02-16 15:49:46 -0500

    removed commented code

--- infrastructure/core/clusters/main.tf
+++ infrastructure/core/clusters/main.tf
@@ -20,7 +20,6 @@ module "cluster" {
   cluster_name = var.service_name
   vpc_id       = data.terraform_remote_state.vpc.outputs.vpc_id["services"]
   subnet_ids   = data.terraform_remote_state.vpc.outputs.private_subnet_ids["services"]
-  #cluster_node_vpc_cidr = data.terraform_remote_state.vpc.outputs.vpc_cidr["services"]
   log_retention_period = 1
 
   providers = {
@@ -44,53 +43,6 @@ module "elb_controller_config" {
     tls         = tls
   }
 }
-
-################################################################################
-#  DNS Node
-################################################################################
-/* module "dns_node" {
-  source                 = "../../.modules/eks-node"
-  project                = var.project
-  environment            = local.environment
-  cluster_name           = module.cluster.cluster_name
-  namespace              = "kube-system"
-  node_name              = "CoreDNS"
-  override_naming_schema = true
-  is_dns_resolver        = true
-  subnet_ids             = data.terraform_remote_state.vpc.outputs.private_subnet_ids["services"]
-  pod_execution_role_arn = module.cluster.pod_exec_role_arn
-  depends_on             = [module.elb_controller_config]
-
-  labels = {
-    k8s-app = "kube-dns"
-  }
-
-  providers = {
-    aws.account = aws.account
-    kubernetes  = kubernetes
-  }
-} */
-
-/* module "elb_controller" {
-  source                 = "../../.modules/eks-node"
-  project                = var.project
-  environment            = local.environment
-  cluster_name           = module.cluster.cluster_name
-  namespace              = "kube-system"
-  node_name              = "aws-load-balancer-controller"
-  override_naming_schema = true
-  is_elb_controller      = true
-  subnet_ids             = data.terraform_remote_state.vpc.outputs.private_subnet_ids["services"]
-  depends_on = [module.elb_controller_config]
-  labels = {
-    "app.kubernetes.io/name" = "aws-load-balancer-controller"
-  }
-
-  providers = {
-    aws.account = aws.account
-    kubernetes  = kubernetes
-  }
-} */
 
 ################################################################################
 #  Namespaces
